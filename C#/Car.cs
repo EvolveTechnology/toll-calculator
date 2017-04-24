d719af3060072d9cb6bd03a49fbfacd417847855
Fredrik Nilsson <fredrik-evolve@users.noreply.github.com>
2017-04-24 10:39:34 +0200

    Add files via upload
    uploaded initial files

---
+++ C#/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator
+{
+    public class Car : Vehicle
+    {
+        public String GetVehicleType()
+        {
+            return "Car";
+        }
+    }
+}
