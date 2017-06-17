e68d681f5d8b6ad29fe09ef0394bba6c6ce4eddf
Alexa DeWit <alexa.dewit@gmail.com>
2017-06-17 11:19:06 +0200

    cleanup, can always be re-introduced from the commit graph

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -84,26 +84,5 @@ public class TollCalculator
             return true;
         }
         return DateSystem.IsPublicHoliday(date, CountryCode.SE);
-
-        /*
-        Removed in favour of library dependency. If further special cases are needed,
-        system can be modified and replaced.
-
-        if (year == 2013)
-        {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
-        return false;
-        */
     }
 }
