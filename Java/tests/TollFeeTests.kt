c926939119767408992f9a6d9d046d7d7201ffba
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-07-03 22:02:48 +0200

    Add tests for toll-free dates

--- Java/tests/TollFeeTests.kt
+++ Java/tests/TollFeeTests.kt
@@ -18,6 +18,36 @@ internal class TollFeeTests {
     }
 
     @TestFactory
+    fun tollFreeDates(): List<DynamicTest> {
+        val normalCar = Car()
+        return listOf(
+                TollFreeDate.NEW_YEARS_DAY,
+                TollFreeDate.MAUNDY_THURSDAY_13,
+                TollFreeDate.GOOD_FRIDAY_13,
+                TollFreeDate.APRIL_FOOL,
+                TollFreeDate.KINGS_BIRTHDAY,
+                TollFreeDate.LABOUR_DAY,
+                TollFreeDate.ASCENSION_EVE_13,
+                TollFreeDate.ASCENSION_DAY_13,
+                TollFreeDate.NATIONAL_DAY_EVE,
+                TollFreeDate.NATIONAL_DAY,
+                TollFreeDate.JULY,
+                TollFreeDate.HALLOWEEN_13,
+                TollFreeDate.CHRISTMAS_EVE,
+                TollFreeDate.CHRISTMAS_DAY,
+                TollFreeDate.BOXING_DAY,
+                TollFreeDate.NEW_YEAR_EVE
+
+        ).map {
+            DynamicTest.dynamicTest("$it") {
+                val calendar = GregorianCalendar(it.year, it.month, it.day, 7, 5)
+                val fee = calculator.getTollFee(normalCar, calendar.time)
+                assertEquals(0, fee)
+            }
+        }
+    }
+
+    @TestFactory
     fun feesAtDifferentTimesOfDay(): List<DynamicTest> {
         val normalCar = Car()
         return mapOf(
@@ -32,11 +62,30 @@ internal class TollFeeTests {
                 TimeOfDay.SIX_PM to 8
         ).map {
             DynamicTest.dynamicTest("${it.key}") {
-                val calendar = GregorianCalendar(2017, 3, 3, it.key.hour, it.key.minute)
+                val calendar = GregorianCalendar(2017, Calendar.MARCH, 3, it.key.hour, it.key.minute)
                 val fee = calculator.getTollFee(normalCar, calendar.time)
                 assertEquals(it.value, fee)
             }
         }
+    }
+
+    private enum class TollFreeDate(val year: Int, val month: Int, val day: Int) {
+        NEW_YEARS_DAY(2013, Calendar.JANUARY, 1),
+        MAUNDY_THURSDAY_13(2013, Calendar.MARCH, 28),
+        GOOD_FRIDAY_13(2013, Calendar.MARCH, 29),
+        APRIL_FOOL(2013, Calendar.APRIL, 1),
+        KINGS_BIRTHDAY(2013, Calendar.APRIL, 30),
+        LABOUR_DAY(2013, Calendar.MAY, 1),
+        ASCENSION_EVE_13(2013, Calendar.MAY, 8),
+        ASCENSION_DAY_13(2013, Calendar.MAY, 9),
+        NATIONAL_DAY_EVE(2013, Calendar.JUNE, 5),
+        NATIONAL_DAY(2013, Calendar.JUNE, 6),
+        JULY(2013, Calendar.JULY, 9),
+        HALLOWEEN_13(2013, Calendar.NOVEMBER, 1),
+        CHRISTMAS_EVE(2013, Calendar.DECEMBER, 24),
+        CHRISTMAS_DAY(2013, Calendar.DECEMBER, 25),
+        BOXING_DAY(2013, Calendar.DECEMBER, 26),
+        NEW_YEAR_EVE(2013, Calendar.DECEMBER, 31)
     }
 
     private enum class TimeOfDay(internal val hour: Int, internal val minute: Int) {
