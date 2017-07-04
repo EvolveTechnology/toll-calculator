52b9f227ab16020a3af4f50d2d18ef15b5f0b226
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-07-04 15:42:08 +0200

    No need to check minute >= 0

--- Java/src/tolls/TimeOfDay.java
+++ Java/src/tolls/TimeOfDay.java
@@ -21,16 +21,16 @@ class TimeOfDay {
     }
 
     int getFee() {
-        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
+        if (hour == 6 && minute <= 29) return 8;
         else if (hour == 6 && minute >= 30) return 13;
-        else if (hour == 7 && minute >= 0) return 18;
-        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
+        else if (hour == 7) return 18;
+        else if (hour == 8 && minute <= 29) return 13;
         else if (hour == 8 && minute >= 30) return 8;
         else if (hour > 8 && hour <= 14) return 8;
-        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16) return 18;
-        else if (hour == 17 && minute >= 0) return 13;
-        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+        else if (hour == 15 && minute <= 29) return 13;
+        else if (hour == 15 || hour == 16) return 18;
+        else if (hour == 17) return 13;
+        else if (hour == 18 && minute <= 29) return 8;
         else return 0;
     }
 }
