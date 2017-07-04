041842677eef55e917271cee0985efe1f483943c
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-07-04 12:58:26 +0200

    Extract code to decide fee by time only

--- Java/src/tolls/TimeOfDay.java
+++ Java/src/tolls/TimeOfDay.java
@@ -5,8 +5,8 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 class TimeOfDay {
-    final int hour;
-    final int minute;
+    private final int hour;
+    private final int minute;
 
     TimeOfDay(Date date) {
         Calendar calendar = GregorianCalendar.getInstance();
@@ -18,5 +18,18 @@ class TimeOfDay {
     private TimeOfDay(int hour, int minute) {
         this.hour = hour;
         this.minute = minute;
+    }
+
+    int getFee() {
+        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
+        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
+        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
+        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
+        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
+        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
+        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
+        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
+        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+        else return 0;
     }
 }
--- Java/src/tolls/TollCalculator.java
+++ Java/src/tolls/TollCalculator.java
@@ -39,19 +39,7 @@ public class TollCalculator {
         if (isTollFreeDate(date) || vehicle.isTollFree()) return 0;
 
         TimeOfDay timeOfDay = new TimeOfDay(date);
-        int hour = timeOfDay.hour;
-        int minute = timeOfDay.minute;
-
-        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-        else return 0;
+        return timeOfDay.getFee();
     }
 
     private Boolean isTollFreeDate(Date date) {
