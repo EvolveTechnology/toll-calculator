7f751ad51407f72c19e457356b690aaf307ae29a
Fredrik Olsson <fredrik.olsson@nilsongroup.com>
2017-08-14 21:14:08 +0200

    [feature_CodeReview]
    *Refactored IsTollFreeDate to use HolidayProvider.IsHoliday

--- Toll-Calculator/TollCalculator.cs
+++ Toll-Calculator/TollCalculator.cs
@@ -71,29 +71,9 @@ public class TollCalculator
         else return 0;
     }
 
-    private Boolean IsTollFreeDate(DateTime date)
+    private bool IsTollFreeDate(DateTime date)
     {
-        int year = date.Year;
-        int month = date.Month;
-        int day = date.Day;
-
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
-
-        if (year == 2013)
-        {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
-        return false;
+        return date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday || DateLibrary.HolidayProvider.IsHoliday(date);
     }
 
     private enum TollFreeVehicles
