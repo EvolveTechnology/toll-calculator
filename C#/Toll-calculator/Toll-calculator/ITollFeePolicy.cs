3f4fcdd02f1cf3364c3d67bb5c11430782ef631b
Oscar Blomqvist <oscar.blomqvist@gmail.com>
2018-03-25 17:32:04 +0200

    Added logic for fee based on time, including tests.

---
+++ C#/Toll-calculator/Toll-calculator/ITollFeePolicy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toll_calculator {
+
+    interface ITollFeePolicy {
+
+        int GetFee(DateTime time);
+
+    }
+}
