b230336514d95b8b7d9d7c360460e0727b94e46d
Jacob Raihle <jraihle@gmail.com>
2018-04-10 21:06:42 +0200

    Add and refactor tests
                                                                        - Adds a test for out-of-order times of passing
                                                                        - Adds a test for passing multiple times per hour for several hours
                                                                        - Refactor tests with utility functions and the new CalendarStream

--- test/se/raihle/tollcalculator/CombinedCalculationTests.java
+++ test/se/raihle/tollcalculator/CombinedCalculationTests.java
@@ -1,10 +1,15 @@
 package se.raihle.tollcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import se.raihle.tollcalculator.test.CalendarStream;
 
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static se.raihle.tollcalculator.test.CalendarBuilder.regularDayAt;
@@ -14,24 +19,22 @@ class CombinedCalculationTests {
 
 	private static final int DAILY_RATE = 60;
 
+	private TollCalculator unit;
+
+	@BeforeEach
+	void setup() {
+		unit = new TollCalculator();
+	}
+
 	@Test
 	void car_passing_two_times_within_an_hour_is_only_charged_the_higher_rate() {
-		TollCalculator unit = new TollCalculator();
-
-		for (int hour = 0; hour < 24; hour++) {
-			for (int minute = 0; minute < 60; minute += 15) {
-				Calendar firstPassing = regularDayAt(hour, minute);
-				Calendar secondPassing = regularDayAt(hour, minute + 30);
-
-				int firstFee = unit.getTollFee(firstPassing.getTime(), REGULAR_CAR);
-				int secondFee = unit.getTollFee(secondPassing.getTime(), REGULAR_CAR);
-
-				int expectedFee = Math.max(firstFee, secondFee);
-				int totalFee = unit.getTollFee(REGULAR_CAR, firstPassing.getTime(), secondPassing.getTime());
-
-				assertEquals(expectedFee, totalFee, errorAt(firstPassing));
+		CalendarStream.from(regularDayAt(0, 0), Calendar.MINUTE, 15).limit(4 * 24).forEach(startingPoint -> {
+			List<Calendar> timesOfPassing = CalendarStream.takeAsList(2, startingPoint, Calendar.MINUTE, 30);
+			if (timesAreOnSameDay(timesOfPassing)) {
+				int expectedFee = highestFeeAmong(timesOfPassing, REGULAR_CAR);
+				assertEquals(expectedFee, totalFeeFor(timesOfPassing, REGULAR_CAR), errorAt(timesOfPassing.get(0)));
 			}
-		}
+		});
 	}
 
 	/*
@@ -40,37 +43,58 @@ class CombinedCalculationTests {
 	@Test
 	@Disabled("Known issue")
 	void car_passing_three_times_within_an_hour_is_only_charged_the_highest_rate() {
-		TollCalculator unit = new TollCalculator();
-
-		for (int hour = 0; hour < 24; hour++) {
-			for (int minute = 0; minute < 60; minute += 15) {
-				Calendar firstPassing = regularDayAt(hour, minute);
-				Calendar secondPassing = regularDayAt(hour, minute + 20);
-				Calendar thirdPassing = regularDayAt(hour, minute + 40);
-
-				int firstFee = unit.getTollFee(firstPassing.getTime(), REGULAR_CAR);
-				int secondFee = unit.getTollFee(secondPassing.getTime(), REGULAR_CAR);
-				int thirdFee = unit.getTollFee(thirdPassing.getTime(), REGULAR_CAR);
-
-				int expectedFee = Math.max(Math.max(firstFee, secondFee), thirdFee);
-				int totalFee = unit.getTollFee(REGULAR_CAR, firstPassing.getTime(), secondPassing.getTime(), thirdPassing.getTime());
-
-				assertEquals(expectedFee, totalFee, errorAt(firstPassing));
+		CalendarStream.from(regularDayAt(0, 0), Calendar.MINUTE, 15).limit(4 * 24).forEach(startingPoint -> {
+			List<Calendar> timesOfPassing = CalendarStream.takeAsList(3, startingPoint, Calendar.MINUTE, 20);
+			if (timesAreOnSameDay(timesOfPassing)) {
+				int expectedFee = highestFeeAmong(timesOfPassing, REGULAR_CAR);
+				assertEquals(expectedFee, totalFeeFor(timesOfPassing, REGULAR_CAR), errorAt(timesOfPassing.get(0)));
 			}
-		}
+		});
+	}
+
+
+	/*
+	 * TODO: Identifies a bug where a car is charged multiple times if it passes twice in an hour after the first hour
+	 * (A different facet of the issue identified in car_passing_three_times_within_an_hour_is_only_charged_the_highest_rate)
+	 */
+	@Test
+	@Disabled("Known issue")
+	void car_passing_twice_per_hour_for_two_hours_is_charged_the_sum_of_higher_rates_for_each_hour() {
+		CalendarStream.from(regularDayAt(0, 0), Calendar.MINUTE, 15).limit(4 * 24).forEach(startingPoint -> {
+			List<Calendar> timesOfPassing = CalendarStream.takeAsList(4, startingPoint, Calendar.MINUTE, 30);
+			List<Calendar> passingsInFirstHour = timesOfPassing.subList(0, 2);
+			List<Calendar> passingsInSecondHour = timesOfPassing.subList(2, 4);
+			if (timesAreOnSameDay(timesOfPassing)) {
+				int expectedFee = highestFeeAmong(passingsInFirstHour, REGULAR_CAR) + highestFeeAmong(passingsInSecondHour, REGULAR_CAR);
+				assertEquals(expectedFee, totalFeeFor(timesOfPassing, REGULAR_CAR), errorAt(timesOfPassing.get(0)));
+			}
+		});
+	}
+
+	/*
+	 * TODO: Identifies unspecified behavior where TollCalculator assumes that times of passing are sorted
+	 * Will also be tripped up by the bug identified in car_passing_twice_per_hour_for_two_hours_is_charged_the_sum_of_higher_rates_for_each_hour
+	 */
+	@Test
+	@Disabled("Known issue")
+	void car_passing_twice_per_hour_for_two_hours_is_charged_the_sum_of_higher_rates_for_each_hour_if_times_are_out_of_order() {
+		CalendarStream.from(regularDayAt(0, 0), Calendar.MINUTE, 15).limit(4 * 24).forEach(startingPoint -> {
+			List<Calendar> timesOfPassing = CalendarStream.takeAsList(4, startingPoint, Calendar.MINUTE, 30);
+			List<Calendar> passingsInFirstHour = timesOfPassing.subList(0, 2);
+			List<Calendar> passingsInSecondHour = timesOfPassing.subList(2, 4);
+			Collections.reverse(timesOfPassing);
+			if (timesAreOnSameDay(timesOfPassing)) {
+				int expectedFee = highestFeeAmong(passingsInFirstHour, REGULAR_CAR) + highestFeeAmong(passingsInSecondHour, REGULAR_CAR);
+				assertEquals(expectedFee, totalFeeFor(timesOfPassing, REGULAR_CAR), errorAt(timesOfPassing.get(0)));
+			}
+		});
 	}
 
 	@Test
 	void car_passing_once_per_hour_for_a_day_is_only_billed_the_daily_rate() {
-		TollCalculator unit = new TollCalculator();
+		List<Calendar> timesOfPassing = CalendarStream.takeAsList(24, regularDayAt(0, 30), Calendar.HOUR_OF_DAY, 1);
 
-		Date[] passings = new Date[24];
-		for (int hour = 0; hour < 24; hour++) {
-			Calendar timeOfPassing = regularDayAt(hour, 30);
-			passings[hour] = timeOfPassing.getTime();
-		}
-
-		int fee = unit.getTollFee(REGULAR_CAR, passings);
+		int fee = unit.getTollFee(REGULAR_CAR, calendarsToDates(timesOfPassing));
 		assertEquals(DAILY_RATE, fee);
 	}
 
@@ -81,16 +105,53 @@ class CombinedCalculationTests {
 	@Test
 	@Disabled("Known issue")
 	void car_passing_once_per_hour_for_two_days_is_billed_double_the_daily_rate() {
-		TollCalculator unit = new TollCalculator();
+		List<Calendar> timesOfPassing = CalendarStream.takeAsList(48, regularDayAt(0, 30), Calendar.HOUR_OF_DAY, 1);
 
-		Date[] passings = new Date[48];
-		for (int hour = 0; hour < 48; hour++) {
-			Calendar timeOfPassing = regularDayAt(hour, 30);
-			passings[hour] = timeOfPassing.getTime();
-		}
-
-		int fee = unit.getTollFee(REGULAR_CAR, passings);
+		int fee = unit.getTollFee(REGULAR_CAR, calendarsToDates(timesOfPassing));
 		assertEquals(DAILY_RATE * 2, fee);
+	}
+
+
+	/**
+	 * Gets the highest individual fee for any of the given passings
+	 */
+	private int highestFeeAmong(List<Calendar> timesOfPassing, Vehicle passer) {
+		return timesOfPassing.stream()
+				.mapToInt(time -> unit.getTollFee(time.getTime(), passer))
+				.max()
+				.getAsInt();
+	}
+
+	/**
+	 * Gets the total fee for the passings as a group
+	 */
+	private int totalFeeFor(List<Calendar> timesOfPassing, Vehicle passer) {
+		return unit.getTollFee(passer, calendarsToDates(timesOfPassing));
+	}
+
+	/**
+	 * Most tests are not concerned with cases when the vehicle passes on multiple days, so we want to filter those out
+	 * @param timesofpassing times of passing sorted from first to last
+	 */
+	private boolean timesAreOnSameDay(List<Calendar> timesofpassing) {
+		Calendar first = timesofpassing.get(0);
+		Calendar last = timesofpassing.get(timesofpassing.size() - 1);
+		return areSame(first, last, Calendar.YEAR) && areSame(first, last, Calendar.DAY_OF_YEAR);
+	}
+
+	/**
+	 * Checks that the two given times have the same value for the given unit.
+	 * The unit is <em>not</em> a minimum granularity - e.g. this function will ignore hours if asked to check minutes.
+	 */
+	private boolean areSame(Calendar a, Calendar b, int unit) {
+		return a.get(unit) == b.get(unit);
+	}
+
+	/**
+	 * Converts a List of Calendars to an array of Dates
+	 */
+	private Date[] calendarsToDates(List<Calendar> calendars) {
+		return calendars.stream().map(Calendar::getTime).collect(Collectors.toList()).toArray(new Date[calendars.size()]);
 	}
 
 	/**
