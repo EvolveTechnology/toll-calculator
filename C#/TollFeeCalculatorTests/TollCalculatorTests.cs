6de842c438c2b984b892a399fab1f6bb4481a491
Robert Sehested <robert.sehested@gmail.com>
2018-04-14 23:02:07 +0200

    Added tests for TollCalculator

--- C#/TollFeeCalculatorTests/TollCalculatorTests.cs
+++ C#/TollFeeCalculatorTests/TollCalculatorTests.cs
@@ -1,14 +1,49 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TollFeeCalculator;
 
 namespace TollFeeCalculatorTests
 {
     [TestClass]
     public class TollCalculatorTests
-    {
+    {    
         [TestMethod]
-        public void TestMethod1()
+        public void GetTollFee_NoFreeDate_NoFreeVehicle()
         {
+            var tollCalculator = new TollCalculator();
+            var date = new DateTime(2018, 01, 02, 7, 35, 0);
+            var vehicle = new Car();
+            int expected = 18;
+
+            int tollFee = tollCalculator.GetTollFee(date, vehicle);
+
+            Assert.AreEqual(expected, tollFee);
+        }
+
+        [TestMethod]
+        public void GetTollFee_FreeDate_NoFreeVehicle()
+        {
+            var tollCalculator = new TollCalculator();
+            var date = new DateTime(2018, 1, 6, 7, 35, 0); // weekend
+            var vehicle = new Car();
+            int expected = 0;
+
+            int tollFee = tollCalculator.GetTollFee(date, vehicle);
+
+            Assert.AreEqual(expected, tollFee);
+        }
+
+        [TestMethod]
+        public void GetTollFee_NoFreeDate_FreeVehicle()
+        {
+            var tollCalculator = new TollCalculator();
+            var date = new DateTime(2018, 01, 02, 7, 35, 0);
+            var vehicle = new Motorbike();
+            int expected = 0;
+
+            int tollFee = tollCalculator.GetTollFee(date, vehicle);
+
+            Assert.AreEqual(expected, tollFee);
         }
     }
 }
