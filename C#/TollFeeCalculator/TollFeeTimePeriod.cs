14f0c1a49990ef30fb7456bf02bf55c2bffb143f
Robert Sehested <robert.sehested@gmail.com>
2018-04-15 11:47:32 +0200

    Added class TollFeeTimePeriod and associated tests

---
+++ C#/TollFeeCalculator/TollFeeTimePeriod.cs
@@ -0,0 +1,34 @@
+﻿namespace TollFeeCalculator
+{
+    /// <summary>
+    /// Represents a daily time period with a specific toll fee.
+    /// </summary>
+    public class TollFeeTimePeriod
+    {
+        private int _startHour;
+        private int _startMinute;
+        private int _endHour;
+        private int _endMinute;
+
+        public int TollFee { get; private set; }
+
+        public TollFeeTimePeriod(int startHour, int startMinute, int endHour, int endMinute, int tollFee)
+        {
+            _startHour = startHour;
+            _startMinute = startMinute;
+            _endHour = endHour;
+            _endMinute = endMinute;
+
+            TollFee = tollFee;
+        }
+
+        public bool IsInTimePeriod(int hour, int minute)
+        {
+            if (hour >= _startHour && hour <= _endHour 
+                && minute >= _startMinute && minute <= _endMinute)
+                return true;
+
+            return false;
+        }
+    }
+}
