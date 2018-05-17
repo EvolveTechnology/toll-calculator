0803e7e5b6405ddb2aa82ba90d3866460e22093b
Srdan Kvrgic <gitkraken@kvrgic.se>
2018-05-17 22:29:26 +0200

    Right. Och så en vettig bastest

--- TollCalculator/src/test/java/se/kvrgic/TollCalculatorTest.java
+++ TollCalculator/src/test/java/se/kvrgic/TollCalculatorTest.java
@@ -1,5 +1,7 @@
 package se.kvrgic;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import static org.junit.Assert.*;
 
@@ -8,10 +10,15 @@ import org.junit.Test;
 public class TollCalculatorTest {
 
     @Test
-    public void getTollFeeBasic() {
-        int tollFee = (new TollCalculator()).getTollFee(new Car(), new Date(114, 2, 2));
-        assertEquals("Toll fee of whatever", 0, tollFee);
+    public void getTollFeeHappyCase() throws Exception {
+        int tollFee = (new TollCalculator()).getTollFee(new Car(), getDate("20130205 07:14"));
+        assertEquals("Toll fee of whatever", 18, tollFee);
     }
     
     
+    
+    private Date getDate(String date) throws ParseException {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyyMMdd HH:mm");
+        return dateFormatter.parse(date);
+    }
 }
