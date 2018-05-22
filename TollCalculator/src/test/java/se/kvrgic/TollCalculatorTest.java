ddd2f8c542096b1dd30496a40d91ac4088e3eca6
Srdan Kvrgic <gitkraken@kvrgic.se>
2018-05-22 21:24:19 +0200

    Röda dagar

--- TollCalculator/src/test/java/se/kvrgic/TollCalculatorTest.java
+++ TollCalculator/src/test/java/se/kvrgic/TollCalculatorTest.java
@@ -2,11 +2,16 @@ package se.kvrgic;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -144,6 +149,25 @@ public class TollCalculatorTest {
         assertEquals("Ingen tull", 0, new TollCalculator().getTollFee(getVehicle("Military"), getDate("20130205 06:00")));
     }
     
+
+    @Test public void getTollFee_RedDatesApartFromWeekends2013() {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate startdatum = LocalDate.parse("20130101", dateFormatter);
+        LocalDate slutdatum  = LocalDate.parse("20131231", dateFormatter);
+        
+        String tollFreeDates = Stream.iterate(startdatum, datum -> datum.plusDays(1))
+                                              .limit(ChronoUnit.DAYS.between(startdatum, slutdatum) + 1)
+                                              .filter(datum -> datum.getYear() == 2013)
+                                              .filter(datum -> !DayOfWeek.SATURDAY.equals(datum.getDayOfWeek()))
+                                              .filter(datum -> !DayOfWeek.SUNDAY  .equals(datum.getDayOfWeek()))
+                                              .filter(datum -> 0 == getTollForDates(dateFormatter.format(datum) + " 06:00"))
+                                              .map(datum -> dateFormatter.format(datum))
+                                              .collect(Collectors.joining(","));
+        assertEquals("Ingen tull", 
+                     "20130101,20130328,20130329,20130401,20130430,20130501,20130508,20130509,20130605,20130606,20130621,20130701,20130702,20130703,20130704,20130705,20130708,20130709,20130710,20130711,20130712,20130715,20130716,20130717,20130718,20130719,20130722,20130723,20130724,20130725,20130726,20130729,20130730,20130731,20131101,20131224,20131225,20131226,20131231", 
+                     tollFreeDates);
+    }
+    
     
     
     private Date getDate(String date) {
@@ -156,7 +180,7 @@ public class TollCalculatorTest {
         }
     }
     
-    private int getTollForDates(String ... dateStrings)  throws ParseException {
+    private int getTollForDates(String ... dateStrings) {
         TollCalculator tollCalculator = new TollCalculator();
         List<Date> dates = Arrays.asList(dateStrings).stream().map(dateString -> getDate(dateString)).collect(Collectors.toList());
         return tollCalculator.getTollFee(new Car(), dates.toArray(new Date[dates.size()]));
