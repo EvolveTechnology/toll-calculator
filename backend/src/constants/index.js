47b8a0c19551479c00ff7ddcaa1c7eecd3212a7c
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-05 22:17:46 +0100

    chore: start to define constants

---
+++ backend/src/constants/index.js
@@ -0,0 +1,9 @@
+export const oneMinute = 60 * 1000;
+export const halfHour = 30 * oneMinute;
+export const oneHour = 60 * oneMinute;
+
+// For this problem, there are no discounts
+// or a need to introduce cents
+// so we can just use continuous numbers to
+// describe money
+export const MAX_FEE = 60;
