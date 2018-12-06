bdba9b179cf490ec1e1dae71b011c84feffc24d3
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-06 06:44:02 +0100

    fix: use time stamp generator and fix length checkers

--- backend/src/services/intervalMaker/tests/intervalMaker.test.js
+++ backend/src/services/intervalMaker/tests/intervalMaker.test.js
@@ -1,23 +1,19 @@
+import intervalMaker from '..';
+import { generateTimeStamps } from '../../../utils';
 import { halfHour, oneHour } from '../../../constants';
-
-const intervalMaker = () => {};
 
 describe('intervalMaker', () => {
   const baseDate = new Date(Date.UTC(2018, 0, 1));
-  const baseDateUnix = baseDate.getTime();
 
   // generate timestamps separated by 30 minutes
-  const everyThirtyMinutes = Array.from(
-    { length: 48 },
-    (_, i) => new Date(baseDateUnix + i * halfHour),
-  );
+  const everyThirtyMinutes = generateTimeStamps(baseDate, halfHour, 48);
 
   // generate timestamps separated by 60 minutes
-  const everyHour = Array.from({ length: 24 }, (_, i) => new Date(baseDateUnix + i * oneHour));
+  const everyHour = generateTimeStamps(baseDate, oneHour, 24);
 
   it('given a set of dates, it makes intervals of a given lenght', () => {
     const twoHours = 2 * oneHour;
-    expect(intervalMaker(everyThirtyMinutes, oneHour)).toHaveLenght(24);
-    expect(intervalMaker(everyHour, twoHours)).toHaveLenght(12);
+    expect(intervalMaker(everyThirtyMinutes, oneHour)).toHaveLength(24);
+    expect(intervalMaker(everyHour, twoHours)).toHaveLength(12);
   });
 });
