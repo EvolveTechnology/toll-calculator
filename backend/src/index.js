1ffd1ed183c5b95caeaf034a53be32a72f999611
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-07 19:42:29 +0100

    feat: fetch holidays for this year :tada:

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import feeByTimeOfDay from './services/feeByTimeOfDay';
-import isTollFreeDate from './services/tollFreeDays';
+import isTollFreeDay from './services/tollFreeDays';
+import apiKey from './key';
 
 const tz = new Date().getTimezoneOffset();
 const now = new Date(new Date().getTime() - tz * 60 * 1000);
 
-console.log('The time is: ', now);
-console.log('Fee:', feeByTimeOfDay(now));
-console.log('Is it a fee free day?', isTollFreeDate(now));
+const year = now.getFullYear();
+const { key } = apiKey;
+
+const endpoint = `https://www.calendarindex.com/api/v1/holidays?country=SE&year=${year}&api_key=${key}`;
+
+const calculateToll = async (day) => {
+  const holidays = await axios.get(endpoint).then(({ data }) => data.response.holidays);
+  const holidayDates = holidays.map(({ date }) => date.split(' ')[0]);
+
+  console.log('The time is: ', day);
+  console.log('Fee:', feeByTimeOfDay(day));
+  console.log('Is it a fee free day?', isTollFreeDay(day, holidayDates));
+};
+
+calculateToll(now);
