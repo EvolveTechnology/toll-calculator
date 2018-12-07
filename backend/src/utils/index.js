0fbee66fe1c5a6aa17852201f311c0cca5f5d1dc
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-07 04:58:27 +0100

    feat: add functional paradigms

--- backend/src/utils/index.js
+++ backend/src/utils/index.js
@@ -20,3 +20,46 @@ export const generateTimeStamps = (start, span, length) => {
  * @return - sorted array of dates from earliest to latest
  */
 export const sortDates = dates => dates.slice(0).sort();
+
+/**
+ * take a result, a function and an index and apply the result to the function
+ * in one of two ways
+ *
+ * A: spread the previous result, so it passes as many arguments
+ * B: simply evaluate the funtion with the previous result
+ *
+ * @param prev - previous result
+ * @param fn - function to receive the result
+ * @param index - a numeric index
+ * @return the resulf of appliying the previous result, by either A or B
+ */
+const folder = (prev, fn, index) => (index === 0 ? fn(...prev) : fn(prev));
+
+/**
+ * pass the a value and its results through a group of functions
+ *
+ * @param functions - funtions
+ * @param args - initial arguments
+ * @return the resulf of passing the initial values through the functions
+ */
+export const pipe = (...fns) => (...args) => fns.reduce(folder, args);
+
+/**
+ *  partially apply arguments to a function
+ *
+ * @param f - funtion
+ * @param a - first group of arguments
+ * @param b - second group of arguments
+ * @return result of appliying the two group of arguments against f
+ */
+export const partial = f => (...a) => (...b) => f(...a, ...b);
+
+/**
+ *  partially apply arguments to a function
+ *
+ * @param f - funtion
+ * @param b - second group of arguments
+ * @param a - first group of arguments
+ * @return result of appliying the two group of arguments against f
+ */
+export const partialRight = f => (...b) => (...a) => f(...a, ...b);
--- backend/src/utils/utils.test.js
+++ backend/src/utils/utils.test.js
@@ -1,4 +1,6 @@
-import { generateTimeStamps, sortDates } from '.';
+import {
+  generateTimeStamps, sortDates, pipe, partial, partialRight,
+} from '.';
 import { oneMinute } from '../constants';
 
 describe('timeStamp', () => {
@@ -29,5 +31,50 @@ describe('sortDates', () => {
   const unOrdered = [...late, ...early];
   it('sorts the dates from earliest to latest', () => {
     expect(sortDates(unOrdered)).toEqual([...early, ...late]);
+  });
+});
+
+describe('pipe', () => {
+  const pass = jest.fn(a => a);
+  const val = 2;
+  const pipeResult = pipe(
+    pass,
+    pass,
+    pass,
+  )(val);
+
+  it('pipes through', () => {
+    expect(pass).toHaveBeenCalledTimes(3);
+    expect(pass).toHaveBeenCalledWith(val);
+  });
+
+  it('returns the expected value', () => {
+    expect(pipeResult).toEqual(val);
+  });
+});
+
+describe('partial', () => {
+  const fn = jest.fn((...args) => args.reduce((acc, val) => acc + val, 0));
+  const firstArgs = [1, 2, 3];
+  const secondArg = 4;
+  const result = partial(fn)(...firstArgs)(secondArg);
+
+  it('allows users to declare arguments in two batches from the left', () => {
+    expect(result).toEqual(10);
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).toHaveBeenCalledWith(1, 2, 3, 4);
+  });
+});
+
+describe('partialRight', () => {
+  const fn = jest.fn((a, b) => a - b);
+  const firstArgs = -1;
+  const secondArg = -1;
+  const result = partialRight(fn)(firstArgs)(secondArg);
+
+  it('allows users to declare arguments in two batches from the right', () => {
+    expect(result).toEqual(0);
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).toHaveBeenCalledWith(-1, -1);
   });
 });
