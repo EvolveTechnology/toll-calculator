0ee02c3ab8af73c210bdac8dbf71d852ad1fe404
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-07 19:35:51 +0100

    feat: add one day definition

--- backend/src/constants/index.js
+++ backend/src/constants/index.js
@@ -1,6 +1,7 @@
 export const oneMinute = 60 * 1000;
 export const halfHour = 30 * oneMinute;
 export const oneHour = 60 * oneMinute;
+export const oneDay = 24 * oneHour;
 
 // For this problem, there are no discounts
 // or a need to introduce cents
