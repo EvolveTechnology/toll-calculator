33da10adb39586abd25efdcacb6f0c9f983afafe
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-08 07:48:37 +0100

    refactor: use fee constants and rename rangeMaker

--- backend/src/services/feeByTimeOfDay/fees.js
+++ backend/src/services/feeByTimeOfDay/fees.js
@@ -1,14 +1,19 @@
-const asMinutes = (h = 0, m = 0) => h * 60 + m;
-const minutesRange = (...from) => (...to) => [from, to].map(hour => asMinutes(...hour));
+import { LOW, MEDIUM, HIGH } from '../../constants';
 
+const asMinutes = (h = 0, m = 0) => h * 60 + m;
+const makeRange = (...from) => (...to) => [from, to].map(hour => asMinutes(...hour));
+
+// We should have a way to allow admins to set ranges and prices
+// unlike in intervalMarker/ which is an implementation to create
+// unique intervals
 export default [
-  { range: minutesRange(6)(6, 29), fee: 8 },
-  { range: minutesRange(6, 30)(6, 59), fee: 13 },
-  { range: minutesRange(7)(7, 59), fee: 18 },
-  { range: minutesRange(8)(8, 29), fee: 13 },
-  { range: minutesRange(8, 30)(14, 59), fee: 8 },
-  { range: minutesRange(15)(15, 29), fee: 13 },
-  { range: minutesRange(15, 30)(16, 29), fee: 18 },
-  { range: minutesRange(17)(17, 59), fee: 13 },
-  { range: minutesRange(18)(18, 29), fee: 8 },
+  { range: makeRange(6)(6, 29), fee: LOW },
+  { range: makeRange(6, 30)(6, 59), fee: MEDIUM },
+  { range: makeRange(7)(7, 59), fee: HIGH },
+  { range: makeRange(8)(8, 29), fee: MEDIUM },
+  { range: makeRange(8, 30)(14, 59), fee: LOW },
+  { range: makeRange(15)(15, 29), fee: MEDIUM },
+  { range: makeRange(15, 30)(16, 29), fee: HIGH },
+  { range: makeRange(17)(17, 59), fee: MEDIUM },
+  { range: makeRange(18)(18, 29), fee: LOW },
 ];
