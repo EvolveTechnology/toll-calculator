bdc804fd245ec7278de54e99e822619c9062ce91
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-09 20:50:51 +0100

    test: include Jan 2nd at midnight

--- backend/src/services/tollFreeDays/tests/tollFreeDays.test.js
+++ backend/src/services/tollFreeDays/tests/tollFreeDays.test.js
@@ -2,9 +2,11 @@ import isTollFreeDate from '..';
 import { weekends, holidays } from './mock';
 
 describe('tollFreeDays', () => {
-  const days = ['2018-01-01', '2018-01-13', '2018-01-15'].map(day => new Date(day));
+  const days = ['2018-01-01', '2018-01-02 00:00:00', '2018-01-13', '2018-01-15'].map(
+    day => new Date(day),
+  );
   it('labels specified holidays as tollfree', () => {
-    expect(days.map(date => isTollFreeDate(date, holidays))).toEqual([true, true, false]);
+    expect(days.map(date => isTollFreeDate(date, holidays))).toEqual([true, false, true, false]);
   });
 
   it('labels weekends as tollfree', () => {
