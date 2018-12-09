3e28e0f3cd9736f31a0197ac8ecbc0b3282ef22f
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-09 20:21:07 +0100

    chore: add mock data

--- backend/src/services/tollCalculator/tests/tollCalculator.test.js
+++ backend/src/services/tollCalculator/tests/tollCalculator.test.js
@@ -1,25 +1,24 @@
 import tollCalculator from '..';
 import groupByDay from '../../groupByDay';
-import { generateTimeStamps, head } from '../../../utils';
-import { oneHour } from '../../../constants';
+import { head } from '../../../utils';
+import dates from './mock';
 
 describe('toll calculator', () => {
-  const baseDate = new Date(new Date(2018, 0, 1));
-
   // generate timestamps separated by 60 minutes
-  const dates = groupByDay(generateTimeStamps(baseDate, oneHour, 24));
+
+  const byDay = groupByDay(dates);
 
   const vehicle = {
     type: 'Car',
   };
-  const holidays = ['2018-01-01 00:00:00', '2018-01-06 00:00:00'];
+  const holidays = ['2018-01-01', '2018-01-06'];
 
   // For this initial test case, our case will cross a toll point every hour
   // and should accumulate more than 60 SEK in fees
   // but will only be charged 60 SEK
   it('calculates the toll for a given vehicle and dates', () => {
-    const passes = head(Object.values(dates));
-    expect(tollCalculator(vehicle, dates, holidays)).toEqual({
+    const passes = head(Object.values(byDay));
+    expect(tollCalculator(vehicle, byDay, holidays)).toEqual({
       chargeablePasses: 24,
       passes,
       totalPasses: passes.length,
