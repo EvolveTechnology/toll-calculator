569e09d8525abad39d8826ea2aed0f052772f481
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-09 20:51:09 +0100

    refactor: use local time and build a pipe

--- backend/src/services/tollFreeDays/index.js
+++ backend/src/services/tollFreeDays/index.js
@@ -1,7 +1,16 @@
-import { head, partialRight, split } from '../../utils';
+import {
+  head, partialRight, split, localTime, pipe,
+} from '../../utils';
 
 const toISO = date => date.toISOString();
 const splitISO = partialRight(split)('T');
+
+const calendarDate = pipe(
+  localTime,
+  toISO,
+  splitISO,
+  head,
+);
 
 const utcWeekend = [0, 6];
 
@@ -15,8 +24,7 @@ const utcWeekend = [0, 6];
  */
 export default (date, holidays) => {
   const isWeekend = utcWeekend.includes(date.getUTCDay());
-  const holidayDate = head(splitISO(toISO(date)));
-  const isHoliday = holidays.includes(holidayDate);
+  const isHoliday = holidays.includes(calendarDate(date));
 
   if (isWeekend || isHoliday) return true;
 
