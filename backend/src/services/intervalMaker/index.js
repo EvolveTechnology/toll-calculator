a80ca69e04446bbab0c0d11137fd48a7da514177
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-09 20:36:59 +0100

    refactor: use localTime

--- backend/src/services/intervalMaker/index.js
+++ backend/src/services/intervalMaker/index.js
@@ -1,3 +1,5 @@
+import { localTime } from '../../utils';
+
 /**
  * Given an array of dates, create intervals one a given length.
  * Dates which fall in between the intervals should not span a new interval
@@ -11,18 +13,19 @@
 export default function intervalMaker(dates, span) {
   return dates.reduce((intervals, curr) => {
     // create the end of probable interval
-    const asUnix = curr.getTime();
+    const start = localTime(curr);
+    const asUnix = start.getTime();
     const afterSpanTime = new Date(asUnix + span);
 
     // if it is the first interval
     if (!intervals.length) {
-      return [{ start: curr, end: afterSpanTime }];
+      return [{ start, end: afterSpanTime }];
     }
 
     // otherwise, grab the last interval
-    const [lastInterval] = intervals.slice(-1);
+    const [{ end }] = intervals.slice(-1);
     // and compare to where we currently are
-    const inInterval = curr < lastInterval.end; // TODO: is this the best comparisson?
+    const inInterval = start < end; // is this the best comparisson?
 
     // if we are inside the interval, pass
     if (inInterval) {
@@ -30,6 +33,6 @@ export default function intervalMaker(dates, span) {
     }
 
     // otherwise add a new interval
-    return intervals.concat([{ start: curr, end: afterSpanTime }]);
+    return intervals.concat([{ start, end: afterSpanTime }]);
   }, []);
 }
