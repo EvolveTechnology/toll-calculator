704012351b416be7c6c1fbcd0c147a0653652131
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-10 16:51:29 +0100

    chore: install create-react-app as frontend

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# Front End
+
+Under construction :)
---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Landing from "./containers/Landing";
+import { register } from "./serviceWorker";
+
+ReactDOM.render(<Landing />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+register();
