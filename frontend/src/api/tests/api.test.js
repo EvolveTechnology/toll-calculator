ab6df99427f40631e7b56d98104b68d7a9c634f2
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-13 16:03:21 +0100

    test: establish testing baseline

---
+++ frontend/src/api/tests/api.test.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { queryOne, queryAll, endpoint } from "..";
+import { mockData, expected, expectedAll } from "./mock";
+
+// This sets the mock adapter on the default instance
+const mock = new MockAdapter(axios);
+
+mock.onPost(`${endpoint}/vehicle`).reply(config => {
+  if (config.data === '"QNX-473"') {
+    return [
+      200,
+      {
+        ...mockData
+      }
+    ];
+  }
+  return [401, { id: null }];
+});
+
+mock.onPost(`${endpoint}/all`).reply(200, [{ ...mockData }]);
+
+describe("query for one", () => {
+  const fallback = {};
+  const callback = jest.fn();
+  const regNum = "QNX-473";
+  const badRegNum = "abc-123";
+
+  it("fetches one vehicle", async () => {
+    await queryOne(regNum, callback, fallback);
+    expect(callback).toHaveBeenCalledWith(expected);
+  });
+
+  it("gracefully handles failure to fetch", async () => {
+    await queryOne(badRegNum, callback, fallback);
+    expect(callback).toHaveBeenCalledWith(fallback);
+  });
+});
+
+describe("query for all", () => {
+  it("fetches one vehicle", async () => {
+    const result = await queryAll();
+    expect(result).toEqual(expectedAll);
+  });
+});
---
+++ frontend/src/api/tests/mock.js
@@ -0,0 +1,73 @@
+export const expected = {
+  allTimeTotalFee: 13,
+  fees: {
+    "2018-11-15": {
+      chargeablePasses: 1,
+      isHoliday: false,
+      isSaturday: false,
+      isSunday: false,
+      isTollFreeVehicle: false,
+      passes: ["2018-11-15 17:51:37"],
+      totalFee: 13,
+      totalPasses: 1
+    }
+  },
+  isTollFree: false,
+  regNum: "QNX-473",
+  results: [
+    {
+      chargeablePasses: 1,
+      day: "2018-11-15",
+      isHoliday: false,
+      isSaturday: false,
+      isSunday: false,
+      isTollFreeVehicle: false,
+      passes: ["2018-11-15 17:51:37"],
+      totalFee: 13,
+      totalPasses: 1
+    }
+  ],
+  showSpinner: false,
+  type: "Truck"
+};
+
+export const expectedAll = [
+  {
+    dates: ["2018-11-15 17:51:37"],
+    fees: {
+      "2018-11-15": {
+        chargeablePasses: 1,
+        isHoliday: false,
+        isSaturday: false,
+        isSunday: false,
+        isTollFreeVehicle: false,
+        passes: ["2018-11-15 17:51:37"],
+        totalFee: 13,
+        totalPasses: 1
+      }
+    },
+    id: "063ad323-8c82-40e6-b509-af8f99c47324",
+    regNum: "QNX-473",
+    totalFee: 13,
+    type: "Truck"
+  }
+];
+
+export const mockData = {
+  dates: ["2018-11-15 17:51:37"],
+  fees: {
+    "2018-11-15": {
+      chargeablePasses: 1,
+      isHoliday: false,
+      isSaturday: false,
+      isSunday: false,
+      isTollFreeVehicle: false,
+      passes: ["2018-11-15 17:51:37"],
+      totalFee: 13,
+      totalPasses: 1
+    }
+  },
+  id: "063ad323-8c82-40e6-b509-af8f99c47324",
+  regNum: "QNX-473",
+  type: "Truck"
+};
