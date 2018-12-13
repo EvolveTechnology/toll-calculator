d76caab582504235f2d51c9590890a048000c8ef
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-13 22:35:01 +0100

    refactor: remove default value for options

--- frontend/src/components/Filter/index.js
+++ frontend/src/components/Filter/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { capitalize } from "../../utils";
 
-export default ({ change, title, options = [] }) => (
+export default ({ change, title, options }) => (
   <div>
     <label htmlFor="type-select">{capitalize(title)}</label>
     <select id="type-select" className="custom-select" onChange={change}>
