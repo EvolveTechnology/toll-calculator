1d6337994d30533ab560cd1c07dbcf3637dff44e
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-13 18:43:46 +0100

    feat: add static constants

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,12 @@
+export const NONE = "None";
+export const HIGHEST = "Highest";
+export const LOWEST = "Lowest";
+export const sortingOptions = [NONE, HIGHEST, LOWEST];
+
+export const SORTING = "sorting";
+export const FILTER_TYPE = "filterType";
+export const TYPE = "type";
+
+export const REFRESH = "Refresh";
+export const LOAD_ALL = "Load All";
+export const ALL = "All";
