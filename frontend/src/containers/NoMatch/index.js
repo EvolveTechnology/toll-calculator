4e5ef872beff8efe4ecea3449b0b68e25d951554
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-13 16:41:57 +0100

    feat: implement 404 no match page

---
+++ frontend/src/containers/NoMatch/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./noMatch.css";
+
+export default () => (
+  <div className="not-found">
+    <h1 className="display-1">404</h1>
+    <p className="lead">That Page was not found.</p>
+  </div>
+);
