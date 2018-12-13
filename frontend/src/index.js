49d1e52af18ee44651210384241a2cdbaafdf802
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-13 08:24:37 +0100

    refactor: use routes as entry point

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Landing from "./containers/Landing";
+import Routes from "./routes";
 import { register } from "./serviceWorker";
 
-ReactDOM.render(<Landing />, document.getElementById("root"));
+ReactDOM.render(<Routes />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
