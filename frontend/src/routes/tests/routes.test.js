19832df511bd74a9be4d4ca1515f31a1e7ce64c4
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-13 16:38:12 +0100

    test: protect router specs

---
+++ frontend/src/routes/tests/routes.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { mount } from "enzyme";
+import { MemoryRouter } from "react-router-dom";
+import Routes from "..";
+
+import Landing from "../../containers/Landing";
+import Dashboard from "../../containers/Dashboard";
+import NoMatch from "../../containers/NoMatch";
+
+describe("It, renders the adequate component for every route", () => {
+  const landing = mount(
+    <MemoryRouter initialEntries={["/"]}>
+      <Routes />
+    </MemoryRouter>
+  );
+
+  const dashboard = mount(
+    <MemoryRouter initialEntries={["/dashboard"]}>
+      <Routes />
+    </MemoryRouter>
+  );
+
+  const noMatch = mount(
+    <MemoryRouter initialEntries={["/30303"]}>
+      <Routes />
+    </MemoryRouter>
+  );
+  it("renders landing", () => {
+    expect(landing.find(Landing)).toHaveLength(1);
+    expect(landing.find(Dashboard)).toHaveLength(0);
+    expect(landing.find(NoMatch)).toHaveLength(0);
+  });
+  it("renders dashboard", () => {
+    expect(dashboard.find(Landing)).toHaveLength(0);
+    expect(dashboard.find(Dashboard)).toHaveLength(1);
+    expect(dashboard.find(NoMatch)).toHaveLength(0);
+  });
+  it("renders 404", () => {
+    expect(noMatch.find(Landing)).toHaveLength(0);
+    expect(noMatch.find(Dashboard)).toHaveLength(0);
+    expect(noMatch.find(NoMatch)).toHaveLength(1);
+  });
+});
