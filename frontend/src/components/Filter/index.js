21d2f34840e1a9c15561f42450426e67633c9ff5
Joseph Chamochumbi <sephxd1234@gmail.com>
2018-12-14 15:54:46 +0100

    feat: disable filter on demand

--- frontend/src/components/Filter/index.js
+++ frontend/src/components/Filter/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { capitalize } from "../../utils";
 
-export default ({ change, title, options }) => (
+export default ({ change, title, options, disabled }) => (
   <div>
     <label htmlFor="type-select">{capitalize(title)}</label>
-    <select id="type-select" className="custom-select" onChange={change}>
+    <select
+      id="type-select"
+      className="custom-select"
+      onChange={change}
+      disabled={disabled}
+    >
       {options.map(type => (
         <option key={type} value={type}>
           {type}
