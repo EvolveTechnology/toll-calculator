52555a68950e3b874508d7f03469c02ea05280af
Albin <a.bramstang@gmail.com>
2019-10-17 23:12:58 +0200

    Empty Main method.

--- TollCalculator/Program.cs
+++ TollCalculator/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-
-namespace TollFeeCalculator
+﻿namespace TollFeeCalculator
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var calculator = new TollCalculator();
-            var vehicle = new Car();
-            var timestamps = new [] { DateTime.Now, DateTime.Now.AddMinutes(30), DateTime.Now.AddHours(-3) };
-            calculator.GetTotalTollFee(vehicle, timestamps);
+
         }
     }
 }
