6a4eb555a1534c1bfc800ddfcbfe82b5bea9c827
Lars Göransson <lars.goransson.77@spray.se>
2020-02-05 17:34:56 +0100

    Add Makefile for running development activities in Docker

--- Elixir/lib/toll_calculator.ex
+++ Elixir/lib/toll_calculator.ex
@@ -1,5 +1,5 @@
 defmodule TollCalculator do
-  def get_toll_fee(vehicle, passages) do
+  def get_toll_fee(_vehicle, _passages) do
     0
   end
 end
--- Elixir/test/toll_calculator_test.exs
+++ Elixir/test/toll_calculator_test.exs
@@ -1,6 +1,6 @@
 defmodule TollCalculatorTest do
   use ExUnit.Case
-  
+
   describe "get_toll_fee/2" do
     test "no passages, returns 0" do
       assert TollCalculator.get_toll_fee(:car, []) == 0
