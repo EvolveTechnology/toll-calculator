9bfa3ce145b53975a06a7f6d8d50033132337377
Lars Göransson <lars.goransson.77@spray.se>
2020-02-05 21:32:44 +0100

    Add fee calculation for time of day

--- Elixir/lib/toll.ex
+++ Elixir/lib/toll.ex
@@ -13,13 +13,39 @@ defmodule Toll do
   @type vehicle :: atom()
 
   @spec calculate_fee(vehicle(), list(NaiveDateTime.t())) :: integer()
+  def calculate_fee(vehicle, _passages) when vehicle in @toll_free_vehicles do
+    0
+  end
+
   def calculate_fee(vehicle, passages) do
     passages
     |> Enum.map(&passage_fee(vehicle, &1))
     |> Enum.sum()
   end
 
-  defp passage_fee(vehicle, _datetime) when vehicle in @toll_free_vehicles do
-    0
+  defp passage_fee(_vehicle, datetime) do
+    time = NaiveDateTime.to_time(datetime)
+    passage_fee_for_time(time)
+  end
+
+  # credo:disable-for-lines:14
+  defp passage_fee_for_time(time) do
+    cond do
+      time_in(time, ~T[06:00:00], ~T[06:30:00]) -> 8
+      time_in(time, ~T[06:30:00], ~T[07:00:00]) -> 13
+      time_in(time, ~T[07:00:00], ~T[08:00:00]) -> 18
+      time_in(time, ~T[08:00:00], ~T[08:30:00]) -> 13
+      time_in(time, ~T[08:30:00], ~T[15:00:00]) -> 8
+      time_in(time, ~T[15:00:00], ~T[15:30:00]) -> 13
+      time_in(time, ~T[15:30:00], ~T[17:00:00]) -> 18
+      time_in(time, ~T[17:00:00], ~T[18:00:00]) -> 13
+      time_in(time, ~T[18:00:00], ~T[18:30:00]) -> 8
+      true -> 0
+    end
+  end
+
+  defp time_in(time, from, to) do
+    (Time.compare(time, from) == :eq or Time.compare(time, from) == :gt) and
+      Time.compare(time, to) == :lt
   end
 end
--- Elixir/test/toll_test.exs
+++ Elixir/test/toll_test.exs
@@ -16,5 +16,58 @@ defmodule TollTest do
       assert Toll.calculate_fee(:foreign, passages) == 0
       assert Toll.calculate_fee(:military, passages) == 0
     end
+
+    test "before 06:00, returns 0" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 05:59:59]]) == 0
+    end
+
+    test "between 06:00 and 06:30, returns 8" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 06:00:00]]) == 8
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 06:29:59]]) == 8
+    end
+
+    test "between 06:30 and 07:00, returns 13" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 06:30:00]]) == 13
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 06:59:59]]) == 13
+    end
+
+    test "between 07:00 and 08:00, returns 18" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 07:00:00]]) == 18
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 07:59:59]]) == 18
+    end
+
+    test "between 08:00 and 08:30, returns 13" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 08:00:00]]) == 13
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 08:29:59]]) == 13
+    end
+
+    test "between 08:30 and 15:00, returns 8" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 08:30:00]]) == 8
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 14:59:59]]) == 8
+    end
+
+    test "between 15:00 and 15:30, returns 13" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 15:00:00]]) == 13
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 15:29:59]]) == 13
+    end
+
+    test "between 15:30 and 17:00, returns 18" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 15:30:00]]) == 18
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 16:59:59]]) == 18
+    end
+
+    test "between 17:00 and 18:00, returns 13" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 17:00:00]]) == 13
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 17:59:59]]) == 13
+    end
+
+    test "between 18:00 and 18:30, returns 8" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 18:00:00]]) == 8
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 18:29:59]]) == 8
+    end
+
+    test "after 18:30, returns 0" do
+      assert Toll.calculate_fee(:car, [~N[2020-02-05 18:30:00]]) == 0
+    end
   end
 end
