38d35b579f7e6be02b923b933df999012b06ce34
Lars Göransson <lars.goransson.77@spray.se>
2020-02-05 17:12:13 +0100

    Generate Elixir application

---
+++ Elixir/lib/toll_calculator.ex
@@ -0,0 +1,18 @@
+defmodule TollCalculator do
+  @moduledoc """
+  Documentation for TollCalculator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TollCalculator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ Elixir/test/toll_calculator_test.exs
@@ -0,0 +1,8 @@
+defmodule TollCalculatorTest do
+  use ExUnit.Case
+  doctest TollCalculator
+
+  test "greets the world" do
+    assert TollCalculator.hello() == :world
+  end
+end
