1e79305653ca34184aa3c33e347fdf97b2b7de62
Caroline Kabat <carolinekabat@gmail.com>
2020-11-20 12:30:29 +0100

    Add get vehicle data and display toll fee receipts

--- reactjs/src/components/Receipt/Receipt.js
+++ reactjs/src/components/Receipt/Receipt.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReceiptRow from "./../ReceiptRow/ReceiptRow";
+import {
+  getFeeForTime,
+  getTotalFeeForDate,
+} from "../../tollcalculator/tollcalculator";
 import "./Receipt.css";
 
-const Receipt = () => {
+const Receipt = ({ date, vehicle }) => {
+  const passages = vehicle.passages[date];
+  const totalFee = getTotalFeeForDate(vehicle, date);
+
+  const rows = passages.map((time, index) => {
+    const fee = getFeeForTime(time);
+    return <ReceiptRow time={time} fee={fee} key={index}></ReceiptRow>;
+  });
+
   return (
     <div data-testid="receipt" className="receipt">
-      <h3 className="receipt-date">2020-11-19</h3>
-      <ul className="receipt-passages-rows">
-        <ReceiptRow time={"12:34:56"} fee={8}></ReceiptRow>
-      </ul>
-      <p className="receipt-total-fee">TOTAL: 8 SEK</p>
+      <h3 className="receipt-date">{date}</h3>
+      <ul className="receipt-passages-rows">{rows}</ul>
+      <p className="receipt-total-fee">TOTAL: {totalFee} SEK</p>
     </div>
   );
 };
