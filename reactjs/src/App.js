57e83ac2084a703979ebb69ee71663f457de4c85
Caroline Kabat <carolinekabat@gmail.com>
2020-11-23 18:15:30 +0100

    Move out fetch request to App level and reorganize mock data

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,13 +1,50 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import ReceiptContainer from "./containers/ReceiptContainer/ReceiptContainer";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>TollFee Calculator</h1>
-      <ReceiptContainer></ReceiptContainer>
-    </div>
-  );
+  const [vehicleData, setVehicleData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const vehicleRegNumber = urlParams.get("reg")
+    ? urlParams.get("reg").toUpperCase()
+    : null;
+
+  useEffect(() => {
+    fetch(`mockDB/${vehicleRegNumber}.json`)
+      .then((res) => res.json())
+      .then(
+        (res) => {
+          setVehicleData(res);
+          setIsLoaded(true);
+        },
+        (error) => {
+          setError(true);
+        }
+      );
+  }, [vehicleRegNumber]);
+
+  if (error) {
+    return (
+      <div className="App">
+        <h1>TollFee Calculator</h1>
+        <div>No data found or missing reg number for vehicle</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="App">
+        <h1>TollFee Calculator</h1>
+        {isLoaded && vehicleData ? (
+          <ReceiptContainer vehicleData={vehicleData}></ReceiptContainer>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- reactjs/src/containers/ReceiptContainer/ReceiptContainer.js
+++ reactjs/src/containers/ReceiptContainer/ReceiptContainer.js
@@ -1,42 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Receipt from "../../components/Receipt/Receipt";
 import "./ReceiptContainer.css";
 
-const ReceiptContainer = () => {
-  const [vehicleData, setVehicleData] = useState([]);
-  const [error, setError] = useState(false);
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    fetch("mockDB/data.json")
-      .then((res) => res.json())
-      .then(
-        (res) => {
-          setVehicleData(res[0]);
-          setIsLoaded(true);
-        },
-        (error) => {
-          setError(true);
-        }
-      );
-  }, []);
-
-  if (error) {
-    return <div>Failed to load data</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div data-testid="receipt-container" className="receipt-container">
-        <h2>Receipts</h2>
-        <div className="receipts">
-          {Object.keys(vehicleData.passages).map((date, index) => (
-            <Receipt date={date} vehicle={vehicleData} key={index}></Receipt>
-          ))}
-        </div>
+const ReceiptContainer = ({ vehicleData }) => {
+  return (
+    <div data-testid="receipt-container" className="receipt-container">
+      <h2>Receipts</h2>
+      <div className="receipts">
+        {Object.keys(vehicleData.passages).map((date, index) => (
+          <Receipt date={date} vehicle={vehicleData} key={index}></Receipt>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default ReceiptContainer;
