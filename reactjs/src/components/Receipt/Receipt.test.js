862d185274093b86e34978b1651df218021ab208
Caroline Kabat <carolinekabat@gmail.com>
2020-11-23 21:07:34 +0100

    Test rendering Receipts with vehicle data

--- reactjs/src/components/Receipt/Receipt.test.js
+++ reactjs/src/components/Receipt/Receipt.test.js
@@ -2,7 +2,14 @@ import { render, screen } from "@testing-library/react";
 import Receipt from "./Receipt";
 
 test("render a receipt", () => {
-  render(<Receipt />);
+  const vehicle = {
+    reg: "ABC123",
+    type: "car",
+    passages: {
+      "2020-12-24": ["15:00:29", "15:35:19"],
+    },
+  };
+  render(<Receipt date="2020-12-24" vehicle={vehicle} />);
   const receipt = screen.getByTestId("receipt");
   expect(receipt).toBeInTheDocument();
 });
--- reactjs/src/containers/ReceiptContainer/ReceiptContainer.test.js
+++ reactjs/src/containers/ReceiptContainer/ReceiptContainer.test.js
@@ -2,7 +2,14 @@ import { render, screen } from "@testing-library/react";
 import ReceiptContainer from "./ReceiptContainer";
 
 test("render container for receipts", () => {
-  render(<ReceiptContainer />);
+  const vehicle = {
+    reg: "ABC123",
+    type: "car",
+    passages: {
+      "2020-12-24": ["15:00:29", "15:35:19"],
+    },
+  };
+  render(<ReceiptContainer vehicleData={vehicle} />);
   const container = screen.getByTestId("receipt-container");
   expect(container).toBeInTheDocument();
 });
