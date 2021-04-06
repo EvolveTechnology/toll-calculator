8ad6b900009d00f5e461e1000d3756a631c7fdaf
Victor Redler <victor.redler@ropocapital.se>
2021-04-06 17:30:47 +0200

    added unittest base

---
+++ C#/Toll.Calculator.UnitTests.Common/UnitTestBase.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+
+namespace Toll.Calculator.UnitTests.Common
+{
+    public abstract class UnitTestBase<T>
+    {
+        private Fixture _fixture;
+
+        protected T SUT => Fixture.Create<T>();
+
+        protected Fixture Fixture => _fixture ?? (_fixture = ConfigureFixture());
+
+        private Fixture ConfigureFixture()
+        {
+            var fixture = new Fixture();
+
+            AddCustomizations(fixture);
+
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
+            return fixture;
+        }
+
+        protected virtual void AddCustomizations(Fixture fixture)
+        {
+            fixture.Customize(new AutoNSubstituteCustomization());
+        }
+    }
+}
