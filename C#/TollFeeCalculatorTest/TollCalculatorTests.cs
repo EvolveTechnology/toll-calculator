e0e86bce4af728f2165f2ac7a934c2f08e730b8c
Classe Filander <classe@filander.se>
2021-05-07 23:38:28 +0200

    Initial clone, new .sln and project structure

---
+++ C#/TollFeeCalculatorTest/TollCalculatorTests.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using TollFeeCalculator;
+
+namespace TollFeeCalculatorTest
+{
+    public class TollCalculatorTests
+    {
+        private readonly TollCalculator calculator = new TollCalculator();
+        private readonly Vehicle car = new Car();
+        private readonly Vehicle motorbike = new Motorbike();
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
