1d1094bcca47f4206ec9da392926e80a3cf8b8e3
Classe Filander <classe@filander.se>
2021-05-09 12:36:31 +0200

    Fixed - New IsTollFreeDate should conform to old 2013 impl fails

--- C#/TollFeeCalculator/TollTariff.cs
+++ C#/TollFeeCalculator/TollTariff.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Nager.Date;
 
 public class TollTariff 
@@ -6,6 +7,21 @@ public class TollTariff
     public Boolean IsTollFreeDate(DateTime date)
     {
         var se = CountryCode.SE;
-        return DateSystem.IsPublicHoliday(date, se) || DateSystem.IsWeekend(date, se);        
+        if (date.Month == 7) return true;
+        if (date.Month == 4 && date.Day == 30) return true;
+        if (date.Month == 6 && date.Day == 05) return true;
+        if (DateSystem.IsWeekend(date, se)) return true;
+        if (DateSystem.IsPublicHoliday(date, se)) return true;
+
+        var holidays = DateSystem.GetPublicHoliday(date.Year, se);
+
+        if (DateSystem.IsPublicHoliday(date.AddDays(1), se))
+        {
+            var holiday = holidays.First(h => h.Date == date.AddDays(1));
+            if (holiday.Name == "Ascension Day") return true;
+            if (holiday.Name == "All Saints' Day") return true;
+            if (holiday.Name == "Good Friday") return true;
+        }
+        return false;
     }
 }
