87e303552f9fe39459aab64e4195115f3b7d0428
Claes Filander <claes.filander@svenskaspel.se>
2021-05-10 22:47:15 +0200

    Fixed - Complexity of GetTollFee is controlled by filtering indata to a maxsize (the solution is now approximate)

--- C#/TollFeeCalculator/TollCalculator.cs
+++ C#/TollFeeCalculator/TollCalculator.cs
@@ -22,9 +22,22 @@ namespace TollFeeCalculator
         public int GetTollFee(IVehicle vehicle, DateTime[] dates)
         {
             if (dates.Any(dt => dates.First().Date != dt.Date)) throw new ArgumentException("Passages at different dates");
+            return CalculateOptimalTollFee(vehicle, dates.Where(d => tollTariff.GetTollFee(d, vehicle) > 0).ToList()); //.ToList());
+            return CalculateOptimalTollFee(vehicle, Filter(dates, vehicle).ToList());
+        }
 
-            var passages = dates.Where(date => tollTariff.GetTollFee(date, vehicle) > 0);
-            return Math.Min(tollTariff.MaxFeePerDay, CalculateOptimalTollFee(vehicle, passages));
+        private IEnumerable<DateTime> Filter(IEnumerable<DateTime> dates, IVehicle vehicle, double resolution = 5.0)
+        {
+            var date = dates.First().Date;
+            for (var minute = 0.0; minute < 24 * 60; minute += resolution)
+            {
+                var dt = date.AddMinutes(minute);
+                var passages = dates.Where(p => Math.Abs((p - dt).TotalMinutes) < resolution / 2 && tollTariff.GetTollFee(p, vehicle) > 0);
+                if (!passages.Any()) continue;
+
+                //return passage with highest toll fee
+                yield return passages.Aggregate((max, x) => tollTariff.GetTollFee(x, vehicle) > tollTariff.GetTollFee(max, vehicle) ? x : max);
+            }
         }
 
         private int CalculateOptimalTollFee(IVehicle vehicle, IEnumerable<DateTime> passages)
