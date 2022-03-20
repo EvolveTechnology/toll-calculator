6f282a50eed944e0bc6923cc23a4fa3fef621e7b
Rizwan <mit.rizwan108@gmail.com>
2022-03-21 00:49:27 +0530

    Getter/Setters removed.

--- toll_fee_calc_app/src/main/java/com/evolve_technology/calculator/controller/model/TollFreeDateDTO.java
+++ toll_fee_calc_app/src/main/java/com/evolve_technology/calculator/controller/model/TollFreeDateDTO.java
@@ -6,17 +6,6 @@ import lombok.Data;
 public class TollFreeDateDTO {
 	String date;
 	Boolean isTollFreeDate;
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	public Boolean getIsTollFreeDate() {
-		return isTollFreeDate;
-	}
-	public void setIsTollFreeDate(Boolean isTollFreeDate) {
-		this.isTollFreeDate = isTollFreeDate;
-	}
+	
 	
 }
--- toll_fee_calc_app/src/main/java/com/evolve_technology/calculator/controller/model/TollFreeVehicleDTO.java
+++ toll_fee_calc_app/src/main/java/com/evolve_technology/calculator/controller/model/TollFreeVehicleDTO.java
@@ -6,17 +6,6 @@ import lombok.Data;
 public class TollFreeVehicleDTO {
 	String name;
 	boolean isTollFreeVehicle;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public boolean isTollFreeVehicle() {
-		return isTollFreeVehicle;
-	}
-	public void setTollFreeVehicle(boolean isTollFreeVehicle) {
-		this.isTollFreeVehicle = isTollFreeVehicle;
-	}
+	
 	
 }
