5aa59d58f6cc14537c837cc4d93bb909e55ad264
Abbas Amiri <abbasamiri@users.noreply.github.com>
2022-04-08 15:56:49 +0430

    Add Constants

---
+++ AbbasAmiriSolution/TollCalculator/Helpers/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TollCalculator.Helpers;
+
+public class Constants
+{
+    public static decimal MaximumFeeForOneDay = 60;
+    public static decimal MinimumFeeDependingOnTheTimeOfDay = 8;
+    public static decimal MaximumFeeDependingOnTheTimeOfDay = 18;
+}
