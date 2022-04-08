ccaabf920b909a8c83732a4d5bc308f5ddcdb00b
Abbas Amiri <abbasamiri@users.noreply.github.com>
2022-04-08 11:57:45 +0430

    Add HolidayCollectionBuilder

---
+++ AbbasAmiriSolution/TollCalculator/Helpers/HolidayCollectionBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace TollCalculator.Helpers;
+
+public class HolidayCollectionBuilder
+{
+    private readonly IList<DateOnly> _holidays = new List<DateOnly>();
+
+    public void Add(DateOnly date)
+    {
+        ArgumentNullException.ThrowIfNull(date);
+
+        if (_holidays.All(c => c != date))
+        {
+            _holidays.Add(date);
+        }
+    }
+
+    public IReadOnlyList<DateOnly> ToReadOnlyList() => _holidays.ToList().AsReadOnly();
+
+    public IList<DateOnly> ToList() => _holidays.ToList(); 
+
+    public IReadOnlyList<DateOnly> ToArray() => _holidays.ToArray(); 
+}
