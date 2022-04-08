bb9e4c04321fa94ac428b168ea89bbc0e06e104e
Abbas Amiri <abbasamiri@users.noreply.github.com>
2022-04-08 15:58:36 +0430

    Add DayTimeFeeCollectionBuilder

---
+++ AbbasAmiriSolution/TollCalculator/Helpers/DayTimeFeeCollectionBuilder.cs
@@ -0,0 +1,39 @@
+﻿using TollCalculator.Models;
+
+namespace TollCalculator.Helpers;
+
+public class DayTimeFeeCollectionBuilder
+{
+    private readonly IList<DayTimeFee> _dayTimeFees = new List<DayTimeFee>();
+
+    public void Add(DayTimeFee dayTimeFee)
+    {
+        ArgumentNullException.ThrowIfNull(dayTimeFee);
+
+        if (_dayTimeFees.Any(c => HasTimeConflict(dayTimeFee, c)))
+        {
+            throw new ArgumentException(null, nameof(dayTimeFee));
+        }
+
+        _dayTimeFees.Add(dayTimeFee);
+    }
+
+    private static bool HasTimeConflict(DayTimeFee first, DayTimeFee second)
+    {
+        return first.Start.IsBetween(second.Start, second.End) ||
+               first.End.IsBetween(second.Start, second.End) ||
+               second.Start.IsBetween(first.Start, first.End) ||
+               second.End.IsBetween(first.Start, first.End);
+    }
+
+    public IReadOnlyList<DayTimeFee> ToReadOnlyList() => _dayTimeFees.ToList().AsReadOnly();
+
+    public IList<DayTimeFee> ToList() => _dayTimeFees.ToList(); 
+
+    public IReadOnlyList<DayTimeFee> ToArray() => _dayTimeFees.ToArray(); 
+
+    public List<IReadOnlyList<(TimeOnly, TimeOnly)>> FindGaps()
+    {
+        throw new NotImplementedException();
+    }
+}
