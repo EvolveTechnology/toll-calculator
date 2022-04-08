d77a2225bd7640821e0f2ffc7218b761be1f8cc4
Abbas Amiri <abbasamiri@users.noreply.github.com>
2022-04-08 11:41:37 +0430

    Create the main functionality of HolidayPolicy

--- AbbasAmiriSolution/TollCalculator/Policies/HolidayPolicy.cs
+++ AbbasAmiriSolution/TollCalculator/Policies/HolidayPolicy.cs
@@ -2,5 +2,15 @@
 
 public class HolidayPolicy
 {
-    
+    private readonly IReadOnlyList<DateOnly> _holidayTable;
+
+    public HolidayPolicy(IReadOnlyList<DateOnly> holidayTable)
+    {
+        _holidayTable = holidayTable;
+    }
+
+    public bool IsHoliday(DateOnly date)
+    {
+        return _holidayTable.Any(c => c == date);
+    }
 }
