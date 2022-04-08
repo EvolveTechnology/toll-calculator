6cfdde8ae10504720fc830920d998d4b4c5dd5c2
Abbas Amiri <abbasamiri@users.noreply.github.com>
2022-04-08 15:57:58 +0430

    Add the main functionality

--- AbbasAmiriSolution/TollCalculator/Policies/DayTimePolicy.cs
+++ AbbasAmiriSolution/TollCalculator/Policies/DayTimePolicy.cs
@@ -1,6 +1,19 @@
-﻿namespace TollCalculator.Policies;
+﻿using TollCalculator.Models;
+
+namespace TollCalculator.Policies;
 
 public class DayTimePolicy
 {
+    private readonly IReadOnlyList<DayTimeFee> _dayTimeFeeTable;
     
+    public DayTimePolicy(IReadOnlyList<DayTimeFee> dayTimeFeeTable)
+    {
+        _dayTimeFeeTable = dayTimeFeeTable;
+    }
+
+    public decimal? Calculate(TimeOnly time)
+    {
+        var timeFee = _dayTimeFeeTable.FirstOrDefault(c => time.IsBetween(c.Start, c.End));
+        return timeFee?.Fee;
+    }
 }
