c0cb7ec405bb145541ed6b14d02b42b3e8dc0eb9
abbas.amiri.developer@outlook.com <abbas.amiri.developer@outlook.com>
2022-04-09 10:52:40 +0430

    Add ConstantsTest

---
+++ AbbasAmiriSolution/TollCalculatorTest/ConstantsTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using TollCalculator.Helpers;
+using Xunit;
+
+namespace TollCalculatorTest;
+
+public class ConstantsTest
+{
+    [Fact]
+    public void ReadJsonFile_Should_Return_Valid_Data()
+    {
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "Data/Constants.json");
+        Constants.ReadJsonFile(path);
+        Assert.Equal(10, Constants.MaximumFeeForOneDay); 
+        Assert.Equal(30, Constants.MaximumFeeDependingOnTheTimeOfDay); 
+        Assert.Equal(20, Constants.MinimumFeeDependingOnTheTimeOfDay);
+        Constants.SetDefaults();
+    }
+}
