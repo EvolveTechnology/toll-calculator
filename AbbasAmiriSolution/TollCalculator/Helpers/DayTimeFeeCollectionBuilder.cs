612641e2ece7a48a74864633f86325b20117d7f9
abbas.amiri.developer@outlook.com <abbas.amiri.developer@outlook.com>
2022-04-09 09:12:44 +0430

    Add ReadJsonFile method

--- AbbasAmiriSolution/TollCalculator/Helpers/DayTimeFeeCollectionBuilder.cs
+++ AbbasAmiriSolution/TollCalculator/Helpers/DayTimeFeeCollectionBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Text.Json.Serialization;
 using TollCalculator.Models;
 
 namespace TollCalculator.Helpers;
@@ -6,6 +7,7 @@ namespace TollCalculator.Helpers;
 public class DayTimeFeeCollectionBuilder
 {
     private readonly List<DayTimeFee> _dayTimeFees = new List<DayTimeFee>();
+
     public void Add(DayTimeFee dayTimeFee)
     {
         ArgumentNullException.ThrowIfNull(dayTimeFee);
@@ -17,7 +19,9 @@ public class DayTimeFeeCollectionBuilder
 
         _dayTimeFees.Add(dayTimeFee);
     }
+
     public void Clear() => _dayTimeFees.Clear();
+
     private static bool HasTimeConflict(DayTimeFee first, DayTimeFee second)
     {
         return first.Start.IsBetween(second.Start, second.End) ||
@@ -25,7 +29,46 @@ public class DayTimeFeeCollectionBuilder
                second.Start.IsBetween(first.Start, first.End) ||
                second.End.IsBetween(first.Start, first.End);
     }
+
     public IReadOnlyList<DayTimeFee> ToReadOnlyList() => _dayTimeFees.ToList().AsReadOnly();
     public IList<DayTimeFee> ToList() => _dayTimeFees.ToList();
     public IReadOnlyList<DayTimeFee> ToArray() => _dayTimeFees.ToArray();
+
+    public void ReadJsonFile(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            throw new ArgumentException("The file path is not specified.");
+        }
+
+        if (!File.Exists(path))
+        {
+            throw new ArgumentException("The does not exist.");
+        }
+
+        var jsonString = File.ReadAllText(path);
+
+        var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        options.Converters.Add(new TimeOnlyConverter());
+        var jsonData = JsonSerializer.Deserialize<IList<JsonModel>>(jsonString, options);
+        if (jsonData == null)
+        {
+            throw new ArgumentException("There is something wrong in the json file.");
+        }
+
+        foreach (var jsonModel in jsonData)
+        {
+            _dayTimeFees.Add(jsonModel.RushHour
+                ? DayTimeFee.CreateRushHour(jsonModel.Start, jsonModel.End)
+                : DayTimeFee.Create(jsonModel.Start, jsonModel.End, jsonModel.Fee));
+        }
+    }
+
+    private class JsonModel
+    {
+        public TimeOnly Start { get; set; }
+        public TimeOnly End { get; set; }
+        public decimal Fee { get; set; }
+        public bool RushHour { get; set; }
+    }
 }
