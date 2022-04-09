73af58a02378bf2f916441328c3ba364b50b3659
abbas.amiri.developer@outlook.com <abbas.amiri.developer@outlook.com>
2022-04-09 09:10:49 +0430

    Add DateOnlyConverter

---
+++ AbbasAmiriSolution/TollCalculator/Helpers/DateOnlyConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TollCalculator.Helpers;
+
+public class DateOnlyConverter : JsonConverter<DateOnly>
+{
+    private readonly string _serializationFormat;
+
+    public DateOnlyConverter() : this(null)
+    {
+    }
+
+    public DateOnlyConverter(string? serializationFormat)
+    {
+        _serializationFormat = serializationFormat ?? "yyyy-MM-dd";
+    }
+
+    public override DateOnly Read(ref Utf8JsonReader reader, 
+        Type typeToConvert, JsonSerializerOptions options)
+    {
+        var value = reader.GetString();
+        return DateOnly.Parse(value!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, 
+        JsonSerializerOptions options)
+        => writer.WriteStringValue(value.ToString(_serializationFormat));
+}
