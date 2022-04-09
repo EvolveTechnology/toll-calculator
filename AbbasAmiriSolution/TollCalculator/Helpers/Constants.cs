b28d0d2125a7d4670166ad477a892b99b832df34
abbas.amiri.developer@outlook.com <abbas.amiri.developer@outlook.com>
2022-04-09 10:49:44 +0430

    Add SetDefaults and ReadJsonFile methods

--- AbbasAmiriSolution/TollCalculator/Helpers/Constants.cs
+++ AbbasAmiriSolution/TollCalculator/Helpers/Constants.cs
@@ -1,8 +1,56 @@
-﻿namespace TollCalculator.Helpers;
+﻿using System.Text.Json;
 
-public static class Constants
+namespace TollCalculator.Helpers;
+
+public class Constants
 {
-    public const decimal MaximumFeeForOneDay = 60;
-    public const decimal MinimumFeeDependingOnTheTimeOfDay = 8;
-    public const decimal MaximumFeeDependingOnTheTimeOfDay = 18;
+    public static decimal MaximumFeeForOneDay;
+    public static decimal MinimumFeeDependingOnTheTimeOfDay;
+    public static decimal MaximumFeeDependingOnTheTimeOfDay;
+
+    static Constants()
+    {
+        SetDefaults();
+    }
+
+    public static void ReadJsonFile(string path)
+    {
+        if (string.IsNullOrEmpty(path))
+        {
+            throw new ArgumentException("The file path is not specified.");
+        }
+
+        if (!File.Exists(path))
+        {
+            throw new ArgumentException("The does not exist.");
+        }
+
+        var jsonString = File.ReadAllText(path);
+
+        var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+
+        var jsonData = JsonSerializer.Deserialize<JsonModel>(jsonString, options);
+        if (jsonData == null)
+        {
+            throw new ArgumentException("There is something wrong in the json file.");
+        }
+
+        MaximumFeeForOneDay = jsonData.MaximumFeeForOneDay;
+        MinimumFeeDependingOnTheTimeOfDay = jsonData.MinimumFeeDependingOnTheTimeOfDay;
+        MaximumFeeDependingOnTheTimeOfDay = jsonData.MaximumFeeDependingOnTheTimeOfDay;
+    }
+
+    public static void SetDefaults()
+    {
+        MaximumFeeForOneDay = 60;
+        MinimumFeeDependingOnTheTimeOfDay = 8;
+        MaximumFeeDependingOnTheTimeOfDay = 18;
+    }
+
+    private class JsonModel
+    {
+        public decimal MaximumFeeForOneDay { get; set; }
+        public decimal MinimumFeeDependingOnTheTimeOfDay { get; set; }
+        public decimal MaximumFeeDependingOnTheTimeOfDay { get; set; }
+    }
 }
