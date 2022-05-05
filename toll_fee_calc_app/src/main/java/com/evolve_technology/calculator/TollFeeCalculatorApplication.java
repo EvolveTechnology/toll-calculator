6f208fcaba448731670d8285cbbda917c2377130
Rizwan <mit.rizwan108@gmail.com>
2022-05-05 22:44:57 +0530

    spring boot removed

--- toll_fee_calc_app/src/main/java/com/evolve_technology/calculator/TollFeeCalculatorApplication.java
+++ toll_fee_calc_app/src/main/java/com/evolve_technology/calculator/TollFeeCalculatorApplication.java
@@ -1,14 +1,9 @@
 package com.evolve_technology.calculator;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TollFeeCalculatorApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SpringApplication.run(TollFeeCalculatorApplication.class, args);
 		
 	}
 
