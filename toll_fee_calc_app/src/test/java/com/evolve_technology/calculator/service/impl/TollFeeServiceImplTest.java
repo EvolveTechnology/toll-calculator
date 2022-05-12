4b88731fff2460ac1edcafb25adcf0003619d0f2
Rizwan <mit.rizwan108@gmail.com>
2022-05-12 17:31:35 +0530

    More Testing.

--- toll_fee_calc_app/src/test/java/com/evolve_technology/calculator/service/impl/TollFeeServiceImplTest.java
+++ toll_fee_calc_app/src/test/java/com/evolve_technology/calculator/service/impl/TollFeeServiceImplTest.java
@@ -200,4 +200,51 @@ class TollFeeServiceImplTest {
 		
 		assertEquals(tollFeeService.getTollFee(inputDates, "Car"), 60);
 	}
+	
+	/*
+	 * Feb 2013 Monday 4th
+	 * 04-02-2013 06:10 && 06:20 && 06:40 && 07:10 && 08:10 && 08:30 && 15:10 && 15:30
+	 * As per the Rule max within an hour is charged that is max of 8,13 plus 18 =31 plus 13 =44 plus 
+	 * max of (13,18) is 18= 62 but it can't go beyond 60 for a day.
+	 * 
+	 * And Feb 2013 Tuesday 5th
+	 * 05-02-2013 06:10
+	 * 
+	 * Monday=60 plus Tuesday=8= 68
+	 */
+	@Test     
+	void testGetTollFeeMondayFebruary2013MultipleDatesMondayPlusTuesday() {
+		List<LocalDateTime> inputDates=Stream.of(LocalDateTime.of(2013, 02, 04, 06, 10),LocalDateTime.of(2013, 02, 04, 06, 20),
+				LocalDateTime.of(2013, 02, 04, 06, 40),LocalDateTime.of(2013, 02, 04, 07, 10),LocalDateTime.of(2013, 02, 04, 8, 10)
+				,LocalDateTime.of(2013, 02, 04, 8, 30),LocalDateTime.of(2013, 02, 04, 15, 10),LocalDateTime.of(2013, 02, 04, 15, 30),
+				LocalDateTime.of(2013, 02, 05, 6, 10)).
+				collect(Collectors.toList());
+		
+		assertEquals(tollFeeService.getTollFee(inputDates, "Car"), 68);
+	}
+	
+	/*
+	 * Feb 2013 Monday 4th
+	 * 04-02-2013 06:10 && 06:20 && 06:40 && 07:10 && 08:10 && 08:30 && 15:10 && 15:30
+	 * As per the Rule max within an hour is charged that is max of 8,13 plus 18 =31 plus 13 =44 plus 
+	 * max of (13,18) is 18= 62 but it can't go beyond 60 for a day.
+	 * 
+	 * And Feb 2013 Tuesday 5th
+	 * 05-02-2013 06:10
+	 * 
+	 * And Feb 2013 Wednesday 6th
+	 * 06-02-2013 17:10 && 18:10
+	 * 
+	 * Monday=60 plus Tuesday=8  plus Wednesday= 13 plus 8 =89
+	 */
+	@Test     
+	void testGetTollFeeMondayFebruary2013MultipleDatesMondayPlusTuesdayPlusWednesday() {
+		List<LocalDateTime> inputDates=Stream.of(LocalDateTime.of(2013, 02, 04, 06, 10),LocalDateTime.of(2013, 02, 04, 06, 20),
+				LocalDateTime.of(2013, 02, 04, 06, 40),LocalDateTime.of(2013, 02, 04, 07, 10),LocalDateTime.of(2013, 02, 04, 8, 10)
+				,LocalDateTime.of(2013, 02, 04, 8, 30),LocalDateTime.of(2013, 02, 04, 15, 10),LocalDateTime.of(2013, 02, 04, 15, 30),
+				LocalDateTime.of(2013, 02, 05, 6, 10),LocalDateTime.of(2013, 02, 06, 17, 10),LocalDateTime.of(2013, 02, 06, 18, 10)).
+				collect(Collectors.toList());
+		
+		assertEquals(tollFeeService.getTollFee(inputDates, "Car"), 89);
+	}
 }
