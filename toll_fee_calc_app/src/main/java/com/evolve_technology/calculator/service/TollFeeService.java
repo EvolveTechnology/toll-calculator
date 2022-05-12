0884e03e4d520a0d3bfffe30b2493714eefbf22b
Rizwan <mit.rizwan108@gmail.com>
2022-05-12 17:47:32 +0530

    code refactored.

--- toll_fee_calc_app/src/main/java/com/evolve_technology/calculator/service/TollFeeService.java
+++ toll_fee_calc_app/src/main/java/com/evolve_technology/calculator/service/TollFeeService.java
@@ -3,8 +3,6 @@ package com.evolve_technology.calculator.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.evolve_technology.calculator.controller.model.TollFee;
-
 public interface TollFeeService {
 
 	public Integer getTollFee(final List<LocalDateTime> list, String vehicle);
